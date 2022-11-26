9773d241a03ee72b250658100057866984f18827
Ezimet Ozkhan <ozkhan@Ezimets-Laptop.local>
2022-11-26 16:01:47 -0500

    fix package

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", .exact("0.6.0")),
         .package(url: "https://github.com/tadija/AEXML.git", .exact("4.4.0")),
         .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", .exact("4.1.0")),
-        .package(url: "https://github.com/realm/realm-cocoa.git", .exact("3.17.3"))
+        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa.git", .exact("3.17.3"))
     ],
     
     targets: [
