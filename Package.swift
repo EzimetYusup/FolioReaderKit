049ca0a65fc4ac2731a5340bd2d8fecba1c34409
Ezimet Ozkhan <ozkhan@Ezimets-Laptop.local>
2022-11-26 16:21:46 -0500

    update dep

--- Package.swift
+++ Package.swift
@@ -9,11 +9,11 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/ZipArchive/ZipArchive.git", .exact("2.1.1")),
+        .package(url: "https://github.com/ZipArchive/ZipArchive.git", .exact("2.2.5")),
         .package(url: "https://github.com/cxa/MenuItemKit.git", .exact("3.1.3")),
-        .package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", .exact("0.6.0")),
+        .package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", .exact("0.6.5")),
         .package(url: "https://github.com/tadija/AEXML.git", .exact("4.4.0")),
-        .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", .exact("4.1.0")),
+        .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", .exact("5.1.0")),
         .package(name: "Realm", url: "https://github.com/realm/realm-cocoa.git", .exact("3.17.0"))
     ],
     
