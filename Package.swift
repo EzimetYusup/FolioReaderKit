03e769a66cf6441907ba2d152fd83c5c501466e3
Ezimet Ozkhan <ozkhan@Ezimets-Laptop.local>
2022-11-26 16:13:11 -0500

    dependency

--- Package.swift
+++ Package.swift
@@ -14,16 +14,11 @@ let package = Package(
         .package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", .exact("0.6.0")),
         .package(url: "https://github.com/tadija/AEXML.git", .exact("4.4.0")),
         .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", .exact("4.1.0")),
-        .package(url: "https://github.com/realm/realm-cocoa.git", .exact("3.17.3"))
+        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa.git", .exact("3.17.3"))
     ],
     
     targets: [
-        .target(name: "FolioReaderKit", dependencies: [.package(url: "https://github.com/ZipArchive/ZipArchive.git", .exact("2.1.1")),
-                                                       .package(url: "https://github.com/cxa/MenuItemKit.git", .exact("3.1.3")),
-                                                       .package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", .exact("0.6.0")),
-                                                       .package(url: "https://github.com/tadija/AEXML.git", .exact("4.4.0")),
-                                                       .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", .exact("4.1.0")),
-                                                       .package(url: "https://github.com/realm/realm-cocoa.git", .exact("3.17.3"))], path: "Source"),
+        .target(name: "FolioReaderKit", dependencies: ["AEXML", "ZipArchive", "FontBlaster", "MenuItemKit", "ZFDragableModalTransition", "Realm"], path: "Source"),
         .testTarget(name: "FolioReaderKitTests", dependencies: ["FolioReaderKit"]),
     ]
 )
