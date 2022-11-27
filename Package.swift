22580db55473ac11e4e6e850b02bf0d6478e9d3b
Ezimet Ozkhan <ozkhan@Ezimets-Laptop.local>
2022-11-27 00:16:48 -0500

    refactored file tree structure

--- Package.swift
+++ Package.swift
@@ -4,31 +4,31 @@ import PackageDescription
 let package = Package(
     name: "FolioReaderKit",
     platforms: [
-                .iOS(.v12),
-        ],
+            .iOS(.v12),
+    ],
     products: [
-        .library(name: "FolioReaderKit", targets: ["FolioReaderKit"]),
+        .library(name: "FolioReaderKit", targets: ["FolioReaderKit"])
     ],
-    
     dependencies: [
-        .package(url: "https://github.com/ZipArchive/ZipArchive.git", .branch("master")),
-        .package(url: "https://github.com/cxa/MenuItemKit.git", .exact("3.0.0")),
-        .package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", .exact("0.6.5")),
-        .package(url: "https://github.com/tadija/AEXML.git", .exact("4.3.3")),
-        .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", .exact("5.1.0")),
-        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa.git", .exact("3.17.0"))
+        .package(url: "https://github.com/drearycold/ZipArchive.git", from: "2.2.5"),
+        .package(url: "https://github.com/cxa/MenuItemKit.git", from: "3.0.0"),
+        .package(url: "https://github.com/drearycold/ZFDragableModalTransition.git", from: "0.6.5"),
+        .package(url: "https://github.com/tadija/AEXML.git", from: "4.3.3"),
+        .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", from: "5.1.0"),
+        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa.git", from: "3.17.3"),
     ],
-    
     targets: [
-        .target(name: "FolioReaderKit", dependencies: ["AEXML", "ZipArchive", "FontBlaster", "MenuItemKit", "ZFDragableModalTransition", .product(name: "RealmSwift", package: "Realm")],
-                path: "Source",
-                exclude: ["Info.plist"],
-                resources: [
-                                .process("Resources/Bridge.js"),
-                                .process("Resources/Style.css"),
-                                .process("Resources/Fonts")
-                            ]
+        .target(
+            name: "FolioReaderKit",
+            dependencies: ["AEXML", "ZipArchive", "FontBlaster", "MenuItemKit", "ZFDragableModalTransition", .product(name: "RealmSwift", package: "Realm")],
+            exclude: ["Info.plist"],
+            resources: [
+                .process("Resources/Bridge.js"),
+                .process("Resources/Style.css"),
+                .process("Resources/Fonts")
+            ]
         ),
-        .testTarget(name: "FolioReaderKitTests", dependencies: ["FolioReaderKit"]),
+        .testTarget(name: "FolioReaderKitTests", dependencies: ["FolioReaderKit"])
     ]
 )
+    
